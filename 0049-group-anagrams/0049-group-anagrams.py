c57227d247609c6afcfb3f1101874c3d7b6ca62e
Nikitha9908 <snikithaa2004@gmail.com>
2025-01-31 12:07:20 +0530

    Time: 16 ms (90.64%), Space: 16.1 MB (62.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,30 +2,16 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs):
-        # Create a dictionary to store groups of anagrams
         anagrams = defaultdict(list)
-        
-        # Iterate through each string in the input list
-        for s in strs:
-            # Sort the string and use the sorted string as the key
-            sorted_str = ''.join(sorted(s))
-            # Append the original string to the corresponding anagram group
-            anagrams[sorted_str].append(s)
-        
-        # Return the values of the dictionary, which are the anagram groups
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))  # Sorting ensures anagrams have the same key
+            anagrams[sorted_word].append(word)
+
         return list(anagrams.values())
 
-# Example test cases
-sol = Solution()
-
-# Test case 1
-print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"])) 
-# Output: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
-
-# Test case 2
-print(sol.groupAnagrams([""])) 
-# Output: [['']]
-
-# Test case 3
-print(sol.groupAnagrams(["a"])) 
-# Output: [['a']]
+# Example Usage
+solution = Solution()
+print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))  # [["bat"],["nat","tan"],["ate","eat","tea"]]
+print(solution.groupAnagrams([""]))  # [[""]]
+print(solution.groupAnagrams(["a"]))  # [["a"]]
